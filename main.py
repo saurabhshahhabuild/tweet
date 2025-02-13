d3b6d2ba7708327fb3514d14f14966cda0723d70
saurabhshahhabuild <saurabhshah@habuild.in>
2025-02-13 16:53:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,31 +7,29 @@ import os
 
 os.environ['GOOGLE_API_KEY'] = "AIzaSyBefhFGtFPGoi4tWIrvNfPk3ePDshxvmAU"
 
-# Create prompt template for generating tweets
-
-tweet_template = "Give me {number} tweets on {topic}"
-
-tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic'])
-
 # Initialize Google's Gemini model
 gemini_model = ChatGoogleGenerativeAI(model = "gemini-1.5-flash-latest")
+
+
+
+# Create prompt template for generating tweets
+
+tweet_template = "Give me {number} tweets on {topic} in {language} language"
+tweet_prompt = PromptTemplate(template = tweet_template, input_variables = ['number', 'topic', 'language'])
 
 
 # Create LLM chain using the prompt template and model
 tweet_chain = tweet_prompt | gemini_model
 
-
-import streamlit as st
+#-------------------------------Streamlit UI------------------------
 
 st.header("Tweet Generator - this is a new change")
-
 st.subheader("Generate tweets using Generative AI")
-
+language = st.selectbox("Pick one", ["hindi", "english"])
 topic = st.text_input("Topic")
-
 number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
 
 if st.button("Generate"):
-    tweets = tweet_chain.invoke({"number" : number, "topic" : topic})
+    tweets = tweet_chain.invoke({"number" : number, "topic" : topic, "language" : language})
     st.write(tweets.content)
     
